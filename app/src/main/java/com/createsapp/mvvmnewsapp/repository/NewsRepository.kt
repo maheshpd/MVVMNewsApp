c21e350f05d40804d30023b63530851348cbc8b2
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-08-24 12:43:34 +0530

    Handling Breaking News Response - MVVM News App

--- app/src/main/java/com/createsapp/mvvmnewsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/createsapp/mvvmnewsapp/repository/NewsRepository.kt
@@ -1,5 +1,9 @@
 package com.createsapp.mvvmnewsapp.repository
 
+import com.createsapp.mvvmnewsapp.api.RetrofitInstance
 import com.createsapp.mvvmnewsapp.db.ArticleDatabase
 
-class NewsRepository(val db: ArticleDatabase)
+class NewsRepository(val db: ArticleDatabase) {
+    suspend fun getBrakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
+}
--- app/src/main/java/com/createsapp/mvvmnewsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/createsapp/mvvmnewsapp/ui/NewsViewModel.kt
@@ -1,6 +1,37 @@
 package com.createsapp.mvvmnewsapp.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.createsapp.mvvmnewsapp.models.NewsResponse
 import com.createsapp.mvvmnewsapp.repository.NewsRepository
+import com.createsapp.mvvmnewsapp.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
-class NewsViewModel(val newsRepository: NewsRepository) : ViewModel()
+class NewsViewModel(val newsRepository: NewsRepository) : ViewModel() {
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var breakingNewsPage = 1
+
+    init {
+        getBreakingNews("us")
+    }
+
+    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val response = newsRepository.getBrakingNews(countryCode, breakingNewsPage)
+        breakingNews.postValue(handleBrakingNewsResponse(response))
+    }
+
+    private fun handleBrakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+
+            }
+        }
+
+        return Resource.Error(response.message())
+    }
+
+}
